ec7f2a2d93cdab8389a48b7cc014233ee6c78e73
Josh Coles <josh@joshcoles.com>
2016-11-08 01:05:40 +0000

    requested and piped image into new file. Success!

---
+++ requestClient.js
@@ -0,0 +1,19 @@
+const request = require('request');
+var fs = require('fs');
+
+request.get('https://sytantris.github.io/http-examples/future.jpg')
+  .on('error', function (err) {
+    console.log("There's an error!: " + err)
+  })
+  .on('response', function (response) {
+    if (response) {
+      console.log("Response received!")
+    }
+    console.log("Response status code is: " + response.statusCode);
+    console.log("Response message is: " + response.statusMessage);
+    console.log("Content Type is: " + response.headers['content-type']);
+    console.log("Content downloading...")
+  })
+  .pipe(fs.createWriteStream('./future.jpeg'));
+
+
